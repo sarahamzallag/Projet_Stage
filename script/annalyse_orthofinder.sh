5f4f31c9fef8d87c1823f63692a2bae54f4e792f
amzallag <sarah.amzallag25@gmail.com>
2026-03-31 15:43:10 +0200

    version 10 - ajout annalyse_orthofinder pour trier les sortie orthofinder pour arriver a la formation d'un plot grace au script python dot_plot.py

---
+++ script/annalyse_orthofinder.sh
@@ -0,0 +1,24 @@
+source="/home/amzallag/stage/agat/fasta_orthofinder/OrthoFinder/Results_Mar26/Orthogroups/Orthogroups.GeneCount.tsv"  # fichier OrthoFinder complet
+chilodiplo="/home/amzallag/stage/Orthogroups.GeneCount.chilo_diplo.tsv"  # tableau contenant seulement les 14 espèces chilo + diplo
+analyse="/home/amzallag/stage/analyse"  # dossier de sortie des résultats
+
+mkdir -p $analyse  # crée le dossier analyse s'il n'existe pas
+
+# extrait les colonnes des 6 chilopodes et des 8 diplopodes
+awk 'BEGIN{FS=OFS="\t"}{print $1,$18,$25,$26,$16,$22,$24,$17,$7,$8,$15,$5,$20,$28,$2}' $source > $chilodiplo
+
+# sélectionne les orthogroups présents chez les 14 espèces
+awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {ok=1; for(i=2;i<=15;i++) if($i==0) ok=0; if(ok) print}' $chilodiplo > $analyse/core_especes.tsv
+
+# sélectionne les orthogroups présents chez tous les chilopodes
+awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {ok=1; for(i=2;i<=7;i++) if($i==0) ok=0; if(ok) print}' $chilodiplo > $analyse/commun_chilo.tsv
+
+# sélectionne les orthogroups présents chez tous les diplopodes
+awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {ok=1; for(i=8;i<=15;i++) if($i==0) ok=0; if(ok) print}' $chilodiplo > $analyse/commun_diplo.tsv
+
+# sélectionne les orthogroups présents chez tous les diplopodes et absents chez tous les chilopodes
+awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {diplo=1; for(i=8;i<=15;i++) if($i==0) diplo=0; chilo_zero=1; for(i=2;i<=7;i++) if($i>0) chilo_zero=0; if(diplo && chilo_zero) print}' $chilodiplo > $analyse/specifi_diplo.tsv
+
+# sélectionne les orthogroups présents chez tous les chilopodes et absents chez tous les diplopodes
+awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {chilo=1; for(i=2;i<=7;i++) if($i==0) chilo=0; diplo_zero=1; for(i=8;i<=15;i++) if($i>0) diplo_zero=0; if(chilo && diplo_zero) print}' $chilodiplo > $analyse/specifi_chilo.tsv
+
---
+++ script/script_cafe.sh
@@ -0,0 +1,36 @@
+
+# chemins
+source_gene="/home/amzallag/stage/agat/fasta_orthofinder/OrthoFinder/Results_Mar26/Orthogroups/Orthogroups.GeneCount.tsv"
+source_tree="/home/amzallag/stage/agat/fasta_orthofinder/OrthoFinder/Results_Mar26/Species_Tree/SpeciesTree_rooted.txt"
+cafe="/home/amzallag/stage/cafe"
+
+# créer dossier
+mkdir -p $cafe
+
+# copier fichiers
+cp $source_gene $cafe/
+cp $source_tree $cafe/
+
+# aller dans dossier
+cd $cafe
+
+# enlever colonne Total (IMPORTANT)
+cut -f1-$(($(head -n1 Orthogroups.GeneCount.tsv | tr '\t' '\n' | wc -l)-1)) Orthogroups.GeneCount.tsv > tmp.txt
+mv tmp.txt Orthogroups.GeneCount.tsv
+
+# charger module
+module load cafe
+
+# créer fichier cafe.txt automatiquement
+cat <<EOF > cafe.txt
+load -i Orthogroups.GeneCount.tsv -t 4 -l log.txt
+tree $(cat SpeciesTree_rooted.txt)
+lambda -s
+report cafe_results
+EOF
+
+
+# lancer cafe
+cafe cafe.txt
+
+echo "CAFE terminé"
